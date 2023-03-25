f29c7624f90dfd58393637cc2c691ddaeddf207b
mag3ia <gpg@e.co.gg>
2023-03-25 22:46:23 +0100

    user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcryptjs');
 
 exports.signUp = async (req, res) => {
     const {name, password} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
     try {
+        const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = await User.create({
             name,
             password: hashedPassword,
@@ -15,6 +15,39 @@ exports.signUp = async (req, res) => {
                 user: newUser,
             },
         });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+}
+
+exports.login = async (req, res) => {
+    const {name, password} = req.body;
+    try {
+        const user = await User.findOne({name});
+        
+        if (!user) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'User not found',
+            });
+        }
+
+        const correct = await bcrypt.compare(password, user.password);
+
+        if(correct) {
+            res.status(200).json({
+                status: 'success',
+                message: 'User logged in',
+            });
+        } else {
+            res.status(400).json({
+                status: 'fail',
+                message: 'Incorrect credentials',
+            });
+        }
     } catch (err) {
         res.status(400).json({
             status: 'fail',
