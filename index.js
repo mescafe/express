1e88087ee16e2a7dfd2317e43d18818e7ade089f
mag3ia <gpg@e.co.gg>
2023-03-25 13:14:46 +0100

    test mongoose

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://root:example@mongo:27017/?authSource=admin')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err)=> console.log('Could not connect to MongoDB...', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
