2ec12cbe53c5aa51fecfe14d0c7c6789e1e23102
mag3ia <gpg@e.co.gg>
2023-03-25 18:54:37 +0100

    setup mongo database

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose.connect('mongodb://root:example@mongo:27017/?authSource=admin')
+const connectWithRetry = () => {
+  mongoose
+  .connect(mongoURL)
   .then(() => console.log('Connected to MongoDB...'))
-  .catch((err)=> console.log('Could not connect to MongoDB...', err));
+  .catch((err)=> console.log('Could not connect to MongoDB...', err)
+  .setTimeout(connectWithRetry, 5000)
+  );
+}
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
